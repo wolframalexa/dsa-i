c00f93a32682b8adfbcf11e1cecfc55a3aa44c8a
wolframalexa <alexa.jakob4@gmail.com>
2019-11-05 13:08:37 -0500

    I compiled hello world woo hoo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*  Alexa Jakob
+    Data Structures and Algorithms I, Fall 2019
+    This program implements instructions from an input file to create, push to, and pop 
+    from stacks and queues, and writes to an output file.
+*/
+
+int main()
+{
+    cout << "Hello World!\n";
+}

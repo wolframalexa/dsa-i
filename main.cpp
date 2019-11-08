3ae05f1d9b7897f32f71e0930c05229fd58035bc
wolframalexa <alexa.jakob4@gmail.com>
2019-11-08 01:19:13 -0500

    doesn't work but saving

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <iostream>  // use standard in/out
+#include <list>      // use lists
+#include <fstream>   // open/close files
 using namespace std;
 
 /*  Alexa Jakob
@@ -9,5 +11,94 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    // list<SimpleList<int> *> listSLi; // all integer stacks and queues
+    // list<SimpleList<double> *> listSLd; // all double stacks and queues
+    // list<SimpleList<string> *> listSLs; // all string stacks and queues
+
 }
+
+void ProcessLine(string line)
+{
+    // read the line
+    // split line into three words: command, name, and value (if any)
+    
+
+    // if command is create, check that the name isn't already present in 
+    // the list. if yes, print error. if no, create it by calling the 
+    // constructor for stack/queue
+
+    // if the command is push, push to the stack or queue
+    // if the command is pop, check that the stack/queue is not empty. 
+        // if yes, print error
+        // if no, pop using remove from start
+
+}
+
+template <typename T> 
+class SimpleList
+{
+    public:
+	SimpleList(string name);
+	virtual void insertAtStart(T value)=0; // push for stacks
+	virtual void insertAtEnd(T value)=0; // push for queues
+	virtual T removeFromStart()=0; // pop for both stacks and queues
+	string getListName();
+
+    private:
+
+        class Node(T value, nextNode)
+        {
+            public:
+            T data = 0; // TODO: replace this with whatever you read in
+            // figure out how to do the pointer thing here
+        }
+
+};
+
+
+template <typename T>
+class Stack : SimpleList<T>
+{
+
+
+    public:
+	void push(T value);
+	void pop();    
+};
+
+template <typename T>
+Stack::push(T value)
+{
+    Stack::insertAtStart(T value);
+};
+
+template <typename T>
+T Stack::pop()
+{
+    Stack::removeFromStart();
+};
+
+
+template <typename T>
+class Queue : SimpleList<T>
+{
+    public:
+	void push(T value);
+	void pop();
+};
+
+template <typename T>
+Queue::push(T value)
+{
+    Queue::insertAtEnd(T value);
+};
+
+template <typename T>
+T Queue::pop(T value)
+{
+    Queue::removeFromStart();
+};
+
+
+
+

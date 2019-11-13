79f9274c466617912897988d960e1ddf5996e416
wolframalexa <alexa.jakob4@gmail.com>
2019-11-12 23:23:03 -0500

    save my work

--- main.cpp
+++ main.cpp
@@ -12,29 +12,49 @@ using namespace std;
     from stacks and queues, and writes to an output file.
 */
 
-ifstream infile;
-ofstream outfile;
-list<SimpleList<int> *> listSLi;    // all integer stacks and queues
-list<SimpleList<double *> listSLd;  // all double stacks and queues
-list<SimpleList<string> *> listSLs; // all string stacks and queues
+// creates a class that sets up a singly linked list from which the stacks
+// and queues are derived
 
-
-int main()
+template <typename T>
+class SimpleList<T>(string name)
 {
-    OpenInputFile(infile);
-    OpenOutputFile(outfile);
+    public:
+        SimpleList(string name);
+        virtual void push(T value)=0;
+        virtual T pop()=0;
+        string getListName();
+        bool isEmpty() const;
 
-    string inLine;
-    while (getline(infile, inLine))
-    {
-	ProcessLine(inLine);
-    }
+    private:
+        struct Node(T data, Node* next)
+        {
+	    T entry = data;
+	    ptr = next;
+        };
 
-    return 0;
+	Node *start;
+        Node *end;
+	
+	string listName;
+
+    protected:
+        void insertAtStart(T data);
+        void insertAtEnd(T data);
+        T removeFromStart();
+};
+
+
+// constructor for SimpleList
+SimpleList<T>::SimpleList(string name)
+{
+    listName = name;
+    start = NULL;
+    end = NULL;
+
 }
 
 
-// prompts user for input file and opens it for reading
+// prompts user for file and opens it for reading
 
 void OpenInputFile(ifstream FileIn)
 {
@@ -54,59 +74,125 @@ void OpenOutputFile(ofstream FileOut)
 }
 
 
-// processes line in file
-void ProcessLine(string line)
+// processes line in file and splits it into three words. If the command is
+// pop, command[2] should be empty
+
+void ReadAndFollowCommand(string line)
 {
-
-
-
-    // read the line
-    // split line into three words: command, name, and value (if any)
+    outfile << "PROCESSING COMMAND: " << line << "\n";
+    istringstream linestream(line);
+    string word;
     
+    int i = 0;
+    while (linestream >> word)
+    {
+	command[i++] = word;
+    }
 
-    // if command is create, check that the name isn't already present in 
-    // the list. if yes, print error. if no, create it by calling the 
-    // constructor for stack/queue
+    // Follow command based on datatype
+    
+    if (command[1].substr(0,1) == "i")
+    {
+	int data = 0;
+	if (command[2].empty() == false)
+	{
+	    data = atoi(command[2].c_str()); //atoi only works on C-style strings
+	    FollowCommand(listSLi, command, data);
+	}
+    }
 
-    // if the command is push, push to the stack or queue
-    // if the command is pop, check that the stack/queue is not empty. 
-        // if yes, print error
-        // if no, pop using remove from start
+    else if (command[1].substr(0,1) == "d")
+    {
+	double data = 0;
+	if (command[2].empty() == false)
+	{
+	    data = atof(command[2].c_str());
+	    FollowCommand(listSLd, command, data);
+	}
+    }
 
+    else if (command[1].substr(0,1) == "s")
+    {
+	string data = command[2];
+	FollowCommand(listSLs, command, data);
+    }
 }
 
 
-// creates a class that sets up a singly linked list from which the stacks
-// and queues are derived
-
-template <typename T> 
-class SimpleList(string name)
+// using the output from the ReadCommand function, follow the appropriate commands
+template <typename T>
+void FollowCommand(list<SimpleList<T> *> listOfLists, string command[], T data)
 {
-    public:
-	SimpleList(string name)
+    if (command[0] == "create")
+    {
+	if (ListSearch(listOfLists,command[1]) == true)
 	{
-	    listName = name;
-	    start = NULL;
-	    end = NULL;
-	};
+	    outfile << "ERROR: This name already exists!\n";
+	}
 
-	virtual void push(T value)=0;
-	virtual T pop()=0;
-	string getListName();
-	bool isEmpty() const;
-
-    private:
-	struct Node
+	else if (command[2] == "queue\0") //input was null terminated
 	{
-	    int data;
-	    node *next;
-	};
+	    Queue<T>* ptr = new Queue<T>(command[1]);
+	    listOfLists.insertAtStart(ptr);
+	}
 
-    protected:
-	void insertAtStart(T data);	// push for stack
-	void insertAtEnd(T data);	// push for queue
-	T removeFromStart();		// pops
-};
+	else if (command[2] = "stack\0")
+	{
+	    Stack<T>* ptr = new Stack<T>(command[1]);
+	    listOfLists.insertAtStart(ptr);
+	}
+    }
+
+    if (command[0] == "push")
+    {
+	if (ListSearch(listofLists, command[1] == false)
+	{
+	    outfile << "ERROR: This name does not exist!\n";
+	}
+
+	else
+	{
+	    command[1].push(data); // TODO: implement using pointers?
+	}
+    }
+
+    if (command[0] == "pop")
+    {
+	if (ListSearch(listofLists, command[1] == false)
+	{
+	    outfile << "ERROR: This name does not exist!\n";
+	}
+
+	else if (command[1].isEmpty() == true)
+	{
+	    outfile << "ERROR: This list is empty!\n";
+	}
+
+	else
+	{
+	    command[1].pop(data); //TODO: implement using pointers?
+	}
+    }
+}
+
+
+template <typename T>
+bool ListSearch(list<SimpleList<T> *> listOfLists, string listname)
+{
+    list<SimpleList<T> *>::iterator it;
+
+    for (it = listOfLists.begin(); it != listOfLists.end(); it++)
+    {
+	string name = (*it)->name;
+	
+	if (name == listname)
+	{
+	    return true;
+	}
+    }
+
+    return false;
+}
 
 
 // getter function for the list name
@@ -118,7 +204,8 @@ string SimpleList<T>::getListName()
 
 
 // checks if the list is empty
-bool isEmpty() const
+template <typename T>
+bool SimpleList<T>::isEmpty() const
 {
     if (start == NULL)
     {
@@ -133,10 +220,8 @@ bool isEmpty() const
 
 // creates Stack derived class
 template <typename T>
-class Stack:SimpleList<T>
+class Stack<T>:SimpleList<T>
 {
-
-
     public:
 	void push(T value);
 	void pop();    
@@ -145,22 +230,22 @@ class Stack:SimpleList<T>
 
 // allows user to push to stacks
 template <typename T>
-Stack::push(T value)
+Stack<T>::push(T value)
 {
-    Stack::insertAtStart(T value);
+    Stack<T>::insertAtStart(T value);
 }
 
 // allows user to pop from stacks
 template <typename T>
-T Stack::pop()
+T Stack<T>::pop()
 {
-    Stack::removeFromStart();
+    Stack<T>::removeFromStart();
 }
 
 
 // creates Queue derived class
 template <typename T>
-class Queue:SimpleList<T>
+class Queue<T>:SimpleList<T>
 {
     public:
 	void push(T value);
@@ -170,18 +255,37 @@ class Queue:SimpleList<T>
 
 // allows user to push to queues
 template <typename T>
-Queue::push(T value)
+Queue<T>::push(T value)
 {
-    Queue::insertAtEnd(T value);
+    Queue<T>::insertAtEnd(T value);
 }
 
 // allows user to pop from queues
 template <typename T>
-T Queue::pop(T value)
+T Queue<T>::pop(T value)
 {
-    Queue::removeFromStart();
+    Queue<T>::removeFromStart();
 }
 
 
+ifstream infile;
+ofstream outfile;
+list<SimpleList<int> *> listSLi;    // all integer stacks and queues
+list<SimpleList<double *> listSLd;  // all double stacks and queues
+list<SimpleList<string> *> listSLs; // all string stacks and queues
 
+
+int main()
+{
+    OpenInputFile(infile);
+    OpenOutputFile(outfile);
+
+    string inLine;
+    while (getline(infile, inLine))
+    {
+        ReadCommand(inLine);
+    }
+
+    return 0;
+}
 
